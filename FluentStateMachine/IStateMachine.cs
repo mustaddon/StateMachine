84e1cf58eba4b12c563ad460c890dc61a73a0d0d
Leonid Salavatov <mustaddon@gmail.com>
2024-02-15 08:42:52 +0300

    1.4.8

--- FluentStateMachine/IStateMachine.cs
+++ FluentStateMachine/IStateMachine.cs
@@ -4,19 +4,25 @@ using System.Threading.Tasks;
 
 namespace FluentStateMachine;
 
-public interface IStateMachine<TState, TEvent> : IStateMachine
-{
-    TState Current { get; }
-    ICollection<TState> States { get; }
-    ICollection<TEvent> Events { get; }
-    Task<bool> IsAvailableStateAsync(TState value, object data = null, CancellationToken cancellationToken = default);
-    Task<bool> IsAvailableEventAsync(TEvent value, object data = null, CancellationToken cancellationToken = default);
-    Task<TResult> TriggerAsync<TResult>(TEvent e, object data = null, CancellationToken cancellationToken = default);
-    Task<bool> JumpToAsync(TState state, object data = null, CancellationToken cancellationToken = default);
-    Task ResetToAsync(TState state, CancellationToken cancellationToken = default);
-}
+public interface IStateMachine<TState, TEvent> : IStateMachine, IStateController<TState>, IEventController<TEvent>;
 
 public interface IStateMachine
 {
     Task ResetAsync(CancellationToken cancellationToken = default);
+}
+
+public interface IStateController<TState>
+{
+    TState Current { get; }
+    ICollection<TState> States { get; }
+    Task<bool> IsAvailableStateAsync(TState value, object data = null, CancellationToken cancellationToken = default);
+    Task<bool> JumpToAsync(TState state, object data = null, CancellationToken cancellationToken = default);
+    Task ResetToAsync(TState state, CancellationToken cancellationToken = default);
+}
+
+public interface IEventController<TEvent>
+{
+    ICollection<TEvent> Events { get; }
+    Task<bool> IsAvailableEventAsync(TEvent value, object data = null, CancellationToken cancellationToken = default);
+    Task<TResult> TriggerAsync<TResult>(TEvent e, object data = null, CancellationToken cancellationToken = default);
 }
--- FluentStateMachine/IStateMachineExtensions.cs
+++ FluentStateMachine/IStateMachineExtensions.cs
@@ -6,45 +6,45 @@ namespace FluentStateMachine;
 
 public static class IStateMachineExtensions
 {
-    public static Task<TResult> TriggerAsync<TState, TEvent, TData, TResult>(this IStateMachine<TState, TEvent> fsm, IFsmEvent<TData, TResult> e, TData data = default, CancellationToken cancellationToken = default)
+    public static Task<TResult> TriggerAsync<TEvent, TData, TResult>(this IEventController<TEvent> fsm, IFsmEvent<TData, TResult> e, TData data = default, CancellationToken cancellationToken = default)
         where TEvent : IFsmEvent
         => fsm.TriggerAsync<TResult>((TEvent)e, data, cancellationToken);
 
-    public static TResult Trigger<TState, TEvent, TData, TResult>(this IStateMachine<TState, TEvent> fsm, IFsmEvent<TData, TResult> e, TData data = default)
+    public static TResult Trigger<TEvent, TData, TResult>(this IEventController<TEvent> fsm, IFsmEvent<TData, TResult> e, TData data = default)
         where TEvent : IFsmEvent
         => TriggerAsync(fsm, e, data).Result;
 
-    public static Task<object> TriggerAsync<TState, TEvent>(this IStateMachine<TState, TEvent> fsm, TEvent e, object data = default, CancellationToken cancellationToken = default)
+    public static Task<object> TriggerAsync<TEvent>(this IEventController<TEvent> fsm, TEvent e, object data = default, CancellationToken cancellationToken = default)
         => fsm.TriggerAsync<object>(e, data, cancellationToken);
 
-    public static object Trigger<TState, TEvent, TResult>(this IStateMachine<TState, TEvent> fsm, TEvent e, object data = null)
+    public static TResult Trigger<TEvent, TResult>(this IEventController<TEvent> fsm, TEvent e, object data = null)
         => fsm.TriggerAsync<TResult>(e, data).Result;
 
-    public static object Trigger<TState, TEvent>(this IStateMachine<TState, TEvent> fsm, TEvent e, object data = null)
+    public static object Trigger<TEvent>(this IEventController<TEvent> fsm, TEvent e, object data = null)
         => fsm.TriggerAsync<object>(e, data).Result;
 
-    public static bool JumpTo<TState, TEvent>(this IStateMachine<TState, TEvent> fsm, TState state, object data = null)
+    public static bool JumpTo<TState>(this IStateController<TState> fsm, TState state, object data = null)
         => fsm.JumpToAsync(state, data).Result;
 
-    public static void ResetTo<TState, TEvent>(this IStateMachine<TState, TEvent> fsm, TState state)
+    public static void ResetTo<TState>(this IStateController<TState> fsm, TState state)
         => fsm.ResetToAsync(state).Wait();
 
-    public static void Reset<TState, TEvent>(this IStateMachine fsm)
+    public static void Reset(this IStateMachine fsm)
     => fsm.ResetAsync().Wait();
 
-    public static bool IsAvailableState<TState, TEvent>(this IStateMachine<TState, TEvent> fsm, TState value, object data = null)
+    public static bool IsAvailableState<TState>(this IStateController<TState> fsm, TState value, object data = null)
         => fsm.IsAvailableStateAsync(value, data).Result;
 
-    public static bool IsAvailableEvent<TState, TEvent>(this IStateMachine<TState, TEvent> fsm, TEvent value, object data = null)
+    public static bool IsAvailableEvent<TEvent>(this IEventController<TEvent> fsm, TEvent value, object data = null)
         => fsm.IsAvailableEventAsync(value, data).Result;
 
-    public static ICollection<TState> GetAvailableStates<TState, TEvent>(this IStateMachine<TState, TEvent> fsm, object data = null)
+    public static ICollection<TState> GetAvailableStates<TState>(this IStateController<TState> fsm, object data = null)
         => fsm.GetAvailableStatesAsync(data).Result;
 
-    public static ICollection<TEvent> GetAvailableEvents<TState, TEvent>(this IStateMachine<TState, TEvent> fsm, object data = null)
+    public static ICollection<TEvent> GetAvailableEvents<TEvent>(this IEventController<TEvent> fsm, object data = null)
         => fsm.GetAvailableEventsAsync(data).Result;
 
-    public static async Task<ICollection<TState>> GetAvailableStatesAsync<TState, TEvent>(this IStateMachine<TState, TEvent> fsm, object data = null, CancellationToken cancellationToken = default)
+    public static async Task<ICollection<TState>> GetAvailableStatesAsync<TState>(this IStateController<TState> fsm, object data = null, CancellationToken cancellationToken = default)
     {
         var result = new HashSet<TState>();
 
@@ -55,7 +55,7 @@ public static class IStateMachineExtensions
         return result;
     }
 
-    public static async Task<ICollection<TEvent>> GetAvailableEventsAsync<TState, TEvent>(this IStateMachine<TState, TEvent> fsm, object data = null, CancellationToken cancellationToken = default)
+    public static async Task<ICollection<TEvent>> GetAvailableEventsAsync<TEvent>(this IEventController<TEvent> fsm, object data = null, CancellationToken cancellationToken = default)
     {
         var result = new HashSet<TEvent>();
 
