b75c6f930282376ef18f6199387aab7028c3ec74
Leonid Salavatov <mustaddon@gmail.com>
2024-02-17 20:50:28 +0300

    examples

--- Examples/Example.ConsoleApp/MediatorExample.Requests.cs
+++ Examples/Example.ConsoleApp/MediatorExample.Requests.cs
@@ -3,28 +3,28 @@ using MediatR;
 
 namespace Example.ConsoleApp;
 
-public class MediatorRequest0 : IFsmEventData<MediatorRequest0, object>, IRequest<object>
+public class MediatorRequest0 : IFsmEvent<MediatorRequest0, object>, IRequest<object>
 {
 
 }
 
-public class MediatorRequest1 : IFsmEventData<MediatorRequest1, double>, IRequest<double>
+public class MediatorRequest1 : IFsmEvent<MediatorRequest1, double>, IRequest<double>
 {
     public int Num { get; set; }
 }
 
-public class MediatorRequest2 : IFsmEventData<MediatorRequest2, object>, IRequest<object>
+public class MediatorRequest2 : IFsmEvent<MediatorRequest2, object>, IRequest<object>
 {
     public bool Bit { get; set; }
 }
 
-public class MediatorRequest3 : IFsmEventData<MediatorRequest3, string>, IRequest<string>
+public class MediatorRequest3 : IFsmEvent<MediatorRequest3, string>, IRequest<string>
 {
     public int Num { get; set; }
     public string? Text { get; set; }
 }
 
-public class MediatorRequest4 : IFsmEventData<MediatorRequest4, object>, IRequest<object>
+public class MediatorRequest4 : IFsmEvent<MediatorRequest4, object>, IRequest<object>
 {
     public int Num { get; set; }
 }
